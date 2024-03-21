345d740841f2533fbb875242a35c3a9f255923c2
santosh4568 <sk5580655@gmail.com>
2024-03-21 12:09:11 +0530

    Added New Method

--- src/main/java/com/demo_spring/helloWorld.java
+++ src/main/java/com/demo_spring/helloWorld.java
@@ -1,13 +1,17 @@
 package com.demo_spring;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class helloWorld {
 	@GetMapping(path = "/")
 	public String print() {
-		return "Hi Sir ! How are you";
+		return "Hi Sir ! ";
 	}
-
+	@GetMapping(path="/{name}")
+	public String printName(@PathVariable String name) {
+		return "Hello " + name +" !";
+	}
 }
